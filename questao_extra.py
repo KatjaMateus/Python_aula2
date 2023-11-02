a3fe74ed696a7f0e79ff0ef5ddaac8c9fadf62e8
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-11-02 18:58:17 -0300

    questao_extra.py

--- questao_extra.py
+++ questao_extra.py
@@ -1,6 +1,6 @@
 # Faça um programa que pede para o usuário cadastrar 5 alunos com nome e nota, e no final mostra o nome e nota do aluno que tirou a maior nota
 
-lista []
+lista = []
 for i in range(5):
     nome = str(input("Digite seu nome: "))
     nota = float(input("Digite sua nota: "))
